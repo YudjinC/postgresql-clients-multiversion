35bebeed0a1d91df7afbe28bccea078656b897ce
BorzilovEV <Evgeniy.Borzilov@gorparkovki.ru>
2025-08-09 19:44:34 +0300

    fix misspell

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,3 @@
-build.sh
-191 B
 #!/usr/bin/env sh
 docker build . -t docker-registry.fitdev.ru:15650/k8s/postgres-client-multiversion:1.0.0
 docker push docker-registry.fitdev.ru:15650/k8s/postgres-client-multiversion:1.0.0
